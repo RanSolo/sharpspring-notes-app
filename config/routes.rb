d45a409f9962a12e194daa19176bfb71c69ff4bc
ransolo <ransolo@me.com>
2020-08-17 14:31:55 -0500

    add-routes-back

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   get 'log_in' => 'sessions#new', :as => 'log_in'
   get 'sign_up' => 'users#new', :as => 'sign_up'
   get 'notes' => 'notes#index', :as => 'notes'
-  # root to: 'sessions#new'
-  # resources :users
-  # resources :sessions
-  # resources :notes
+  root to: 'sessions#new'
+  resources :users
+  resources :sessions
+  resources :notes
 
-  # get :note_email, to: 'notes#note_email', as: :note_email
+  get :note_email, to: 'notes#note_email', as: :note_email
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
