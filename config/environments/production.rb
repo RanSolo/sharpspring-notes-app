03d7f8bc25e3b2a0fa601d4e61a008f8aa6be73a
ransolo <ransolo@me.com>
2020-08-17 17:54:16 -0500

    use-real-email-for-default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_options = { from: 'OtherNotes@app.com' }
+  config.action_mailer.default_options = { from: 'ransolo@me.com' }
 
   config.action_mailer.delivery_method = :sendmail
   # Defaults to:
