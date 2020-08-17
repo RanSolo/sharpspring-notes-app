c3ebb499b336626e5dcfe688b138379478d45a19
ransolo <ransolo@me.com>
2020-08-17 18:13:10 -0500

    send-email-to-current-user

--- app/mailers/note_mailer.rb
+++ app/mailers/note_mailer.rb
@@ -4,8 +4,7 @@ class NoteMailer < ApplicationMailer
   def note_email
     @user = params[:user]
     @note = params[:note]
-    @email = params[:send_to_email]
     @body = @note.body
-    mail(to: @email, from: @user.email, subject: @note.title)
+    mail(to: @user.email, subject: @note.title)
   end
 end
